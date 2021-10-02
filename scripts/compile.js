93066e649cd947f5816a04b9c856c3ad55328927
hectormontesinosp <hector.montesinos@copernic.tech>
2021-10-02 18:07:32 +0200

    improvements

---
+++ scripts/compile.js
@@ -0,0 +1,40 @@
+const path = require('path');           // For work with filesystem
+const fs = require('fs');               // For work with disk files
+const solc = require('solc'); 
+const chalk = require('chalk');          
+
+// Find the file
+const contractPath = path.resolve(__dirname, "../contracts", "UsersContract.sol");
+
+// Read their font-code
+const source = fs.readFileSync(contractPath, 'utf-8');
+
+// console.log(source);
+//const {interface, bytecode} = solc.compile(source, 1).contracts[':UsersContract'];
+
+//console.log(solc.compile(source, 1));
+// console.log(chalk.green(bytecode));
+// console.log(chalk.cyan(interface));
+
+var input = {
+    language: 'Solidity',
+    sources: {
+      'UsersContract.sol': {
+        content: source,
+      },
+    },
+    settings: {
+      outputSelection: {
+        '*': {
+          '*': [ '*' ]
+        }
+      }
+    }
+  };
+
+var output = JSON.parse(solc.compile(JSON.stringify(input)));
+// console.log(output.bytecode);
+
+const bytecode = output.contracts['UsersContract.sol'];
+
+//console.log(chalk.green(bytecode));
