e0636e8cda232f5aaaca1b15775c727ccd6b968b
hectormontesinosp <hector.montesinos@copernic.tech>
2021-10-03 18:18:22 +0200

    improvements in comments

--- scripts/compile.js
+++ scripts/compile.js
@@ -1,5 +1,5 @@
-const path = require('path');           // For work with filesystem
-const fs = require('fs');               // For work with disk files
+const path = require('path');           // For working with filesystem
+const fs = require('fs');               // For working with disk files
 const solc = require('solc'); 
 const chalk = require('chalk');          
 
@@ -9,29 +9,11 @@ const contractPath = path.resolve(__dirname, "../contracts", "UsersContract.sol"
 // Read their font-code
 const source = fs.readFileSync(contractPath, 'utf-8');
 
-// console.log(source);
 const {interface, bytecode} = solc.compile(source, 1).contracts[':UsersContract'];
+// Bytecode => Código compilado traducido por la EVM
+// ABI (Application Binary Interface) => Define un contrato y sus funciones, da forma guardandolo en un fichero JSON.
+// se utilizará en librerias como Web3 para inicializar instancias del contrato y ejecutar transacciones contra él
 
-//console.log(solc.compile(source, 1));
 console.log(chalk.green(bytecode));
 console.log(chalk.cyan(interface));
 
-/*var input = {
-    language: 'Solidity',
-    sources: {
-      'UsersContract.sol': {
-        content: source,
-      },
-    },
-    settings: {
-      outputSelection: {
-        '*': {
-          '*': [ '*' ]
-        }
-      }
-    }
-  };
-
-var output = JSON.parse(solc.compile(JSON.stringify(input)));*/
-
-// exports.bytecode = output.contracts['UsersContract.sol']['UsersContract'].evm.bytecode.object;
