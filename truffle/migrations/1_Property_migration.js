86aeb7f87cd16ee1a615fe67c7d5299f870a77fc
hmonpa <hmonpa@gmail.com>
2021-11-09 16:35:07 +0100

    delete duplicates

--- truffle/migrations/1_Property_migration.js
+++
@@ -1,5 +0,0 @@
-let PropertiesContract = artifacts.require("./Properties");
-
-module.exports = function(deployer){
-    deployer.deploy(PropertiesContract);
-}
--- truffle/migrations/old/2_usersContract_migration.js
+++
@@ -1,5 +0,0 @@
-let UsersContract = artifacts.require("./UsersContract.sol");
-
-module.exports = function(deployer){
-    deployer.deploy(UsersContract);
-}
