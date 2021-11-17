7ebab8c28a2a7afc4a0e4a0c11ed9f5935260674
hmonpa <hmonpa@gmail.com>
2021-11-17 17:55:50 +0100

    improvements in tests, findByAddr function to be solved

--- contracts/Auth.sol
+++ contracts/Auth.sol
@@ -17,10 +17,11 @@ contract Auth {
     }
 
     mapping(uint256 => User) public users;
+    mapping(address => User) public usersByAddr;
 
     constructor() public 
     {
-        signUp("Héctor", "hmonpa@gmail.com", "pass12345");
+        signUp("Héctor", "hmonpa@gmail.com", "pass123456");
     }
 
     event newUser(
@@ -37,9 +38,12 @@ contract Auth {
         string _password
     ) public returns (bool) 
     {
-        require(users[usersCounter].addr != msg.sender);
+        // require(users[usersCounter].addr != msg.sender);
         address _address = msg.sender;
+
         users[usersCounter] = User(_address, _name, _email, _password, false);
+        usersByAddr[_address] = User(_address, _name, _email, _password, false);
+        
         usersCounter++;
         emit newUser(_address, _name, _email, _password, false);
         return true;
@@ -48,27 +52,46 @@ contract Auth {
     function signIn(address _address, string memory _password) public returns (bool)
     {
         if (
-            keccak256(abi.encodePacked(users[usersCounter].password)) 
+            keccak256(abi.encodePacked(usersByAddr[_address].password)) 
             == keccak256(abi.encodePacked(_password)) 
             && 
-            users[usersCounter].addr 
+            usersByAddr[_address].addr 
             == _address
         )
         {
             users[usersCounter].isLoggedIn = true;
-            return users[usersCounter].isLoggedIn;
+            usersByAddr[_address].isLoggedIn = true;
+            return true;
         } else {
             return false;
         }
     }
 
-    function checkIsUserLogged(uint _usersCounter) public view returns (bool, string memory)
+    function findByAddr(address _address) public view returns (uint)
     {
-        return (users[_usersCounter].isLoggedIn, users[_usersCounter].name);
+        for (uint i = 0; i < usersCounter; i++)
+        {
+            if (_address == users[usersCounter].addr) return i;
+        }
+    }
+
+    function checkIsUserLogged(uint _usersCounter) public view returns (bool)
+    {
+        return (users[_usersCounter].isLoggedIn);
+    }
+
+    function checkIsUserLoggedByAddr(address _address) public view returns (bool)
+    {
+        return (usersByAddr[_address].isLoggedIn);
     }
 
     function logout(uint _usersCounter) public 
     {
         users[_usersCounter].isLoggedIn = false;
+    }
+
+    function logoutByAddr(address _address) public 
+    {
+        usersByAddr[_address].isLoggedIn = false;
     }
 }
--- test/Auth.test.js
+++ test/Auth.test.js
@@ -8,6 +8,7 @@ contract('Auth', () => {
     it('Auth contract deployed successfully', async() => {
         const address = this.Auth.address;
 
+        // @ de contrato válida
         assert.notEqual(address, null);
         assert.notEqual(address, undefined);
         assert.notEqual(address, 0x0);
@@ -16,16 +17,54 @@ contract('Auth', () => {
     });
 
     it('get users list and individual', async() => {
+        // Nuevo usuario
         let signUp = await this.Auth.signUp("Example", "example@example.com", "pass12345");
         
+        // Número de usuarios total
         const userCounter = await this.Auth.usersCounter();
-        assert.equal(userCounter.toNumber(), 2);
+        assert.equal(userCounter.toNumber(), 2);            
 
-        const user = signUp.logs[0].args;
+        // Comprobación de datos
+        const firstUser = await this.Auth.users(0);
+        
+        assert.equal(firstUser.name, "Héctor");
+        assert.equal(firstUser.email, "hmonpa@gmail.com");
+        assert.equal(firstUser.password, "pass123456");
+        assert.equal(firstUser.isLoggedIn, false);
 
-        assert.equal(user.name, "Example");
-        assert.equal(user.email, "example@example.com");
-        assert.equal(user.password, "pass12345");
+        // Segunda comprobación de datos
+        const secondUser = signUp.logs[0].args;
+        assert.equal(secondUser.name, "Example");
+        assert.equal(secondUser.email, "example@example.com");
+        assert.equal(secondUser.password, "pass12345");
+        assert.equal(secondUser.isLoggedIn, false);
+    });
+
+    it('Users are logged successfully', async() => {
+        let UserExample = await this.Auth.users(1);
+        let UserAddr = UserExample.addr;
+        let UserPwd = UserExample.password;
+
+        let signIn = await this.Auth.signIn(UserAddr, UserPwd);
+        assert.equal(UserAddr, "0x627306090abaB3A6e1400e9345bC60c78a8BEf57");
+        assert.equal(UserPwd, "pass12345");
+
+        let checkLoginByAddr = await this.Auth.checkIsUserLoggedByAddr(UserAddr);
+        assert.equal(checkLoginByAddr, true);
+
+        // let checkLogin = await this.Auth.checkIsUserLogged(1);
+        // assert.equal(checkLogin, true);
+    });
+
+    it('find specific user', async() => {
+        const usersCounter = await this.Auth.usersCounter();
+        
+        let newUser = await this.Auth.signUp("Another User", "anotheruser@example.com", "pass12345");
+        newUser = newUser.logs[0].args;
+        let newUserAddr = newUser.addr;
+
+        let index = await this.Auth.findByAddr(newUserAddr);
+        assert.equal(index, 0);
     });
 
 });
