3f0ad692d70e1195e0260a971abcf4a8567bcc16
hmonpa <hmonpa@gmail.com>
2021-11-20 17:46:11 +0100

    delete old files

--- old/ipfs.js
+++
@@ -1,8 +0,0 @@
-import * as IPFS from 'ipfs-core'
-
-async function upload() {
-    const ipfs = await IPFS.create()
-    const { cid } = await ipfs.add('Hello world')
-    console.info(cid)
-}
-module.exports = upload
